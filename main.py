32fa854b515faa38b2d49ea5fc252a1a65484763
woznot <53011091+woznot@users.noreply.github.com>
2020-03-19 15:11:24 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+
+#set up individual teams
+f_e =["Liam", "Pav", "Tiago"]
+b_e=["Duncan", "Amitabh", "Sylvain", "Puneet"]
+d_o=["Gavan", "Zong-Pei"]
+p_t=["Satnam", "Tarig", "Michal"]
+f_o=["Tracy", "Zoe", "Tobias"]
+
+#groupe the teams together. 
+whole_group = [f_e, b_e, d_o, p_t, f_o]
+
+#create an array with the number of groups we want to split the team into. 
+group_sets =[]
+number_of_groups =3
+for t in range (0, number_of_groups,1):
+    group_sets.append([])
+g_n=0   #set starting position as 1
+
+# start with each team, and distribute them first before going to the next team
+for i in range(0,len(whole_group)):
+    j=len(whole_group[i])
+    while j>0:
+        if j>1:
+            a=random.randrange(0,j-1,1)
+        else:
+            a=0
+        group_sets[g_n].append(whole_group[i][a])
+        whole_group[i].pop(a) #remove person from list
+        
+        #check is we have gone through all the groups.  start from the beginning is we have
+        if g_n<(number_of_groups-1): 
+            g_n=g_n+1 
+        else:
+            g_n=0
+            
+        j=len(whole_group[i])
+
+#print the sets.
+for b in range (0, len(group_sets)):
+    print (group_sets[b])
+    
